f41a6234c392a3145dd757ea00665230b0b31c67
tobiju <t.jueschke@googlemail.com>
2015-11-30 19:17:31 +0100

    Added table support.

--- share/prismjs/main.js
+++ share/prismjs/main.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     var codes = document.getElementsByTagName('code');
-
     for (var i = 0; i < codes.length; i++) {
-        codes[i].className += ' line-numbers';
+        if (document.documentElement.classList) {
+            codes[i].classList.add("line-numbers");
+        }
+    }
+
+    var tables = document.getElementsByTagName('table');
+    for (var i = 0; i < tables.length; i++) {
+        if (document.documentElement.classList) {
+            tables[i].classList.add("table");
+        }
+
+        var oldHtml = tables[i].outerHTML;
+        tables[i].outerHTML = "<div class='table-responsive'>" + oldHtml + "</div>";
     }
 });
